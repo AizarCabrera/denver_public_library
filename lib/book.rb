2e74b764f08607b0abe1f5610da875d7c4a2010e
Aizar <aizar.cabrera@icloud.com>
2018-09-30 12:25:38 -0600

    adds iteration 1 finished

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,11 @@
 class Book
+  attr_reader :author_first_name, :author_last_name, :title, :publication_date
 
-
+  def initialize(book_data)
+    # binding.pry
+    @author_first_name  = book_data[:author_first_name]
+    @author_last_name   = book_data[:author_last_name]
+    @title              = book_data[:title]
+    @publication_date   = book_data[:publication_date].split(", ").last
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,35 @@
 require './test/test_helper'
+require './lib/book'
 
 class BookTest < Minitest::Test
+  def setup
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
 
+  def test_it_exists
+    assert_instance_of Book, @book
+  end
+
+  def test_it_has_author_first_name
+    assert_equal "Harper", @book.author_first_name
+  end
+
+  def test_it_has_author_last_name
+    assert_equal "Lee", @book.author_last_name
+  end
+
+  def test_it_can_have_a_different_author
+    book_with_different_author = Book.new({author_first_name: "Sara", author_last_name: "Maas", title: "Throne of Glass", publication_date: "July 11, 1960"})
+    assert_equal "Sara", book_with_different_author.author_first_name
+    assert_equal "Maas", book_with_different_author.author_last_name
+  end
+
+  def test_it_has_a_title
+    assert_equal "To Kill a Mockingbird", @book.title
+  end
+
+  def test_it_has_a_publication_date
+    assert_equal "1960", @book.publication_date
+  end
 
 end
